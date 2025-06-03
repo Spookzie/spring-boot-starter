b745f6ccf2ecbd8a2e3bc8ecf635ca6db2eee50a
Vansh Bansal <vb.vansh04@gmail.com>
2025-06-03 23:28:57 +0530

    Handling nested objects

--- src/main/java/com/spookzie/database/config/MapperConfig.java
+++ src/main/java/com/spookzie/database/config/MapperConfig.java
@@ -1,6 +1,7 @@
 package com.spookzie.database.config;
 
 import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -16,6 +17,9 @@ public class MapperConfig
     @Bean
     public ModelMapper modelMapper()
     {
-        return new ModelMapper();
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.LOOSE);
+
+        return modelMapper;
     }
 }
